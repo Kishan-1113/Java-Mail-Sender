a089241d0ee072d9043fdf2b6202bcbe84dad2b1
Kishan-1113 <bhadrakishan717@gmail.com>
2026-05-10 00:38:43 +0530

    some improvements

--- src/main/java/com/example/emailsender/Services/MailWorker.java
+++ src/main/java/com/example/emailsender/Services/MailWorker.java
@@ -1,6 +1,8 @@
 package com.example.emailsender.Services;
 
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -18,21 +20,39 @@ public class MailWorker {
     @Autowired
     private BlockingQueue<EmailSend> queue;
 
+    private final ExecutorService executor = Executors.newFixedThreadPool(3);
+
     @PostConstruct
-    public void startWorker() {
-        Thread worker = new Thread(() -> {
-            while (true) {
-                try {
-                    EmailSend request = queue.take();
-                    eMailService.sendSimpleEmail(
-                            request.getSendTo(),
-                            "Test Email",
-                            "Hello, your email is working successfully!");
-                } catch (Exception e) {
-                    e.printStackTrace();
+    public void startWorkers() {
+
+        for (int i = 1; i <= 3; i++) {
+
+            int workerId = i;
+
+            executor.submit(() -> {
+
+                while (true) {
+
+                    try {
+
+                        EmailSend request = queue.take();
+
+                        System.out.println(
+                                "Worker-" + workerId +
+                                        " processing " +
+                                        request.getSendTo());
+
+                        eMailService.sendSimpleEmail(
+                                request.getSendTo(),
+                                "Test Email",
+                                "Hello, your email is working successfully!");
+
+                    } catch (Exception e) {
+
+                        e.printStackTrace();
+                    }
                 }
-            }
-        });
-        worker.start();
+            });
+        }
     }
 }
